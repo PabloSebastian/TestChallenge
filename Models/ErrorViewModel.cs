8692178f78ca9cc56bdb2073db530047df43e0a7
Pablo Sebastián García <pablosebas33@gmail.com>
2019-03-22 08:08:16 -0300

    Project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestChallenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
